0d8c1141db8a03c5af825ac4d37196f05d15539b
Robson Jorge <robsonsouzajorge@hotmail.com>
2024-03-11 19:24:43 -0300

    :lipstick: style: Adicionar estilos de redimensionamento vertical ao container

--- src/components/Textarea/styles.js
+++ src/components/Textarea/styles.js
@@ -8,7 +8,16 @@ export const Container = styled.textarea`
   color: ${({ theme }) => theme.COLORS.WHITE};
 
   border: none;
-  resize: none;
+  resize: vertical;
+
+  &::-webkit-resizer {
+    border-color: ${({ theme }) => theme.COLORS.GRAY_100};
+    border-style: solid;
+    border-top-width: 0;
+    border-bottom-width: 2px;
+    border-right-width: 2px;
+    border-left-width: 0;
+  }
 
   margin-bottom: 8px;
   border-radius: 10px;
